40a2938e60e04be29bb7e9a3e9c58dc31ee9523e
Diego Riquelme <diegoriquelme81@gmail.com>
2021-08-27 19:52:09 -0300

    se remueve lineas y se agrega comentario

--- configs.php
+++ configs.php
@@ -1,12 +1,10 @@
 <?php
 
-
+// archivo de constantes del sistema
 const ERROR_LOGIN_CODE = 1;
 const ERROR_LOGIN_MENSAJE = "Login Incorrrecto. Intente nuevamente";
 
-
 const MENSAJE_CODE = 20;
-CONST MENSAJE_NECESITA_LOGIN = "Acceso denegado. Necesita loguearse.";
-
+const MENSAJE_NECESITA_LOGIN = "Acceso denegado. Necesita loguearse.";
 
 ?>
